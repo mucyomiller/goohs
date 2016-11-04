bd3715d74cd8d8c6c21436af24cdfca8c60f0fdf
Mucyo F Miller <mucyomiller@gmail.com>
2016-11-04 18:12:55 +0200

    add migrations&models doctors,patients,appointments

---
+++ database/migrations/2016_11_04_155459_create_patients_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('patient_type');
+            $table->string('name');
+            $table->string('gender');
+            $table->string('birthDate');
+            $table->string('bloodGroup');
+            $table->string('symptoms');
+            $table->string('mobile');
+            $table->string('email');
+            $table->string('address');
+            $table->string('image');
+            $table->string('size');
+            $table->string('type');
+            $table->integer('employee_id');
+            $table->integer('seat_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('patients');
+    }
+}
---
+++ database/migrations/2016_11_04_160550_create_appointments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
