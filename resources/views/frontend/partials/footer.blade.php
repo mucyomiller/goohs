380cdff805751d8a6574ba49717509df343164b4
Mucyo F Miller <mucyomiller@gmail.com>
2016-11-04 16:05:21 +0200

    add partials views source

---
+++ resources/views/frontend/partials/footer.blade.php
@@ -0,0 +1,107 @@
+	<!-- Footer Starts -->
+		<footer class="main-footer">
+		<!-- Footer Area Starts -->
+			<div class="footer-area">
+			<!-- Nested Container Starts -->
+				<div class="container">
+					<div class="row">
+					<!-- Hosptial Information Starts -->
+						<div class="col-md-3 col-sm-4 col-xs-12">
+							<h4>Hosptial Information</h4>
+							<p>
+								Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including.
+							</p>
+							<ul class="list-unstyled address-list">
+								<li class="clearfix address">
+									<i class="fa fa-home"></i>
+									Kigali Rwanda, Rou de l'armee  UR-CST.
+								</li>
+								<li class="clearfix">
+									<i class="fa fa-fax"></i>
+									(250) - 7xx xxx xxx
+								</li>
+								<li class="clearfix">
+									<i class="fa fa-phone"></i>
+									(250) - 7xx xxx xxx
+								</li>
+								<li class="clearfix">
+									<i class="fa fa-envelope"></i>
+									<a href="mailto:info@goohs.com">info@goohs.com</a>
+								</li>
+							</ul>
+						</div>
+					<!-- Hosptial Information Ends -->
+					<!-- Services Starts -->
+						<div class="col-md-2 col-sm-4 col-xs-12">
+							<h4>Services</h4>
+							<ul class="list-unstyled">
+								<li><a href="#"><i class="fa fa-angle-right"></i> Pediatric Clinic</a></li>
+								<li><a href="#"><i class="fa fa-angle-right"></i> Diagnosis Clinic</a></li>
+								<li><a href="#"><i class="fa fa-angle-right"></i> Cardiac Clinic</a></li>
+								<li><a href="#"><i class="fa fa-angle-right"></i> Laboratory Analysis</a></li>
+								<li><a href="#"><i class="fa fa-angle-right"></i> Dental Clinic</a></li>
+								<li><a href="#"><i class="fa fa-angle-right"></i> Gynecological Clinic</a></li>
+								<li><a href="#"><i class="fa fa-angle-right"></i> Psychological</a></li>
+							</ul>
+						</div>
+					<!-- Services Ends -->
+					<!-- Twitter Starts -->
+						<div class="col-md-3 col-sm-4 col-xs-12">
+							<h4>Twitter</h4>
+							<ul class="list-unstyled tweets-list">
+								<li>
+									<i class="fa fa-twitter"></i>
+									Our Hospitals Website Launched.
+									<a href="#">http://t.co/xyz12abc</a>
+								</li>
+							</ul>
+						</div>
+					<!-- Twitter Ends -->
+					<!-- Signup Newsletter Starts -->
+						<div class="col-md-4 col-xs-12 newsletter-block">
+							<h4>Signup Newsletter</h4>
+							<form action="#" class="newsletter">
+								<div class="form-group">
+									<input type="text" class="form-control" placeholder="Your Name">
+								</div>
+								<div class="form-group">
+									<input type="email" class="form-control" placeholder="Enter your Email Address">
+								</div>
+								<button type="submit" class="btn btn-lg btn-block btn-secondary">Submit</button>
+							</form>
+						</div>
+					<!-- Signup Newsletter Ends -->
+					</div>
+				</div>
+			<!-- Nested Container Ends -->
+			</div>
+		<!-- Footer Area Ends -->
+		<!-- Copyright Starts -->
+			<div class="copyright">
+				<div class="container clearfix">
+					<p class="pull-left">
+						&copy; Copyright 2016. AlL Rights Reserved By <span>Computer Techs Geeks</span>
+					</p>
+					<ul class="list-unstyled list-inline pull-right">
+						<li><a href="#">Terms Of Services</a></li>
+						<li><a href="#">Privacy</a></li>
+						<li><a href="contact.html">Contact Us</a></li>
+					</ul>
+				</div>
+			</div>
+		<!-- Copyright Ends -->
+		</footer>
+	<!-- Footer Ends -->
+	<!-- Template JS Files -->
+	<script src="{{asset('js/jquery-1.11.3.min.js')}}"></script>
+	<script src="{{asset('js/jquery-migrate-1.2.1.min.js')}}"></script>	
+	<script src="{{asset('js/bootstrap.min.js')}}"></script>
+	<script src="{{asset('js/plugins/camera/js/jquery.mobile.customized.min.js')}}"></script>
+	<script src="{{asset('js/plugins/camera/js/jquery.easing.1.3.js')}}"></script>
+	<script src="{{asset('js/plugins/camera/js/camera.min.js')}}"></script>	
+	<script src="{{asset('js/plugins/shuffle/jquery.shuffle.modernizr.min.js')}}"></script>
+	<script src="{{asset('js/plugins/magnific-popup/jquery.magnific-popup.min.js')}}"></script>
+	<script src="https://maps.googleapis.com/maps/api/js"></script>	
+	<script src="{{asset('js/custom.js')}}"></script>	
+	</body>
+</html>
---
+++ resources/views/frontend/partials/header.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		
+		<title>GOOHS</title>
+		
+		<!-- Bootstrap -->
+		<link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+		
+		<!-- Google Web Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic" rel="stylesheet" type="text/css">
+		<link href="https://fonts.googleapis.com/css?family=Roboto:400,100,300,100italic,300italic,400italic,500,500italic,700,700italic,900,900italic" rel="stylesheet" type="text/css">
+		
+		<!-- Template CSS Files  -->
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet">
+		<link href="{{asset('js/plugins/camera/css/camera.css')}}" rel="stylesheet">
+		<link href="{{asset('js/plugins/magnific-popup/magnific-popup.css')}}" rel="stylesheet">
+		<link href="{{asset('css/style.css')}}" rel="stylesheet">
+		<link href="{{asset('css/responsive.css')}}" rel="stylesheet">
+		
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<!-- Fav and touch icons -->
+		<link rel="shortcut icon" href="images/fav.png">
+		
+	</head>
+	<body>
+	<!-- Header Starts -->
+		<header class="main-header">
+		<!-- Nested Container Starts -->
+			<div class="container">
+			<!-- Top Bar Starts -->
+				<div class="top-bar hidden-sm hidden-xs">
+					<div class="row">
+						<div class="col-sm-6 col-xs-12">
+							<span>Opening Hours:</span>  Mon To Fri - 7:00 - 18:00, Sat &amp; Sun - 08:00 - 12:00
+						</div>
+						<div class="col-sm-6 col-xs-12">
+							<ul class="list-unstyled list-inline">
+								<li><a href="mailto:care@yourhosptialsname.com">
+									<i class="fa fa-envelope-o"></i>
+									care@goohs.com
+								</a></li>
+								<li><i class="fa fa-phone"></i> Call Us: (250)-788-XXX-XXX</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			<!-- Top Bar Ends -->
+			<!-- Navbar Starts -->
+				<nav id="nav" class="navbar navbar-default" role="navigation">
+					<div class="container-fluid">
+					<!-- Navbar Header Starts -->
+						<div class="navbar-header">
+						<!-- Collapse Button Starts -->
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						<!-- Collapse Button Ends -->
+						<!-- Logo Starts -->
+							<a href="index.html" class="navbar-brand">
+								<i class="fa fa-heartbeat"></i>
+								Goohs
+							</a>
+						<!-- Logo Ends -->
+						</div>
+					<!-- Navbar Header Ends -->
+					<!-- Navbar Collapse Starts -->
+						<div class="navbar-collapse collapse">
+							<ul class="nav navbar-nav navbar-right">
+								<li class="active"><a href="index.html">Home</a></li>
+								<li><a href="services.html">Services</a></li>
+								<li><a href="#" class="dropdown-toggle" data-toggle="dropdown">make Appointment</a></li>
+								<li><a href="about.html">About</a></li>
+								<li><a href="contactus.html">Contact</a></li>
+							</ul>
+						</div>
+					<!-- Navbar Collapse Ends -->
+					</div>
+				</nav>
+			<!-- Navbar Ends -->
+			</div>
+		<!-- Nested Container Ends -->
+		</header>
+	<!-- Header Ends -->
