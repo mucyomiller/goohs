5f223fed95d68d203fe4f719be8c31f83ab429ca
Mucyo F Miller <mucyomiller@gmail.com>
2016-11-25 19:16:22 +0200

    add partials

---
+++ resources/views/dashboard/partials/footer.blade.php
@@ -0,0 +1,51 @@
+<!-- BEGIN CORE JS FRAMEWORK-->
+    
+<!--[if lt IE 9]>
+<script src="assets/plugins/respond.js"></script>
+<![endif]-->
+
+<script src="{{ asset('dashboard/assets/plugins/jquery-1.8.3.min.js')}}" type="text/javascript"></script>
+<script src="{{ asset('dashboard/assets/plugins/jquery-ui/jquery-ui-1.10.1.custom.min.js')}}" type="text/javascript"></script>
+<script src="{{ asset('dashboard/assets/plugins/boostrapv3/js/bootstrap.min.js')}}" type="text/javascript"></script>
+<script src="{{ asset('dashboard/assets/plugins/breakpoints.js')}}" type="text/javascript"></script>
+<script src="{{ asset('dashboard/assets/plugins/jquery-unveil/jquery.unveil.min.js')}}" type="text/javascript"></script>
+<script src="{{ asset('dashboard/assets/plugins/jquery-block-ui/jqueryblockui.js')}}" type="text/javascript"></script>
+<script src="{{ asset('dashboard/assets/plugins/jquery-lazyload/jquery.lazyload.min.js')}}" type="text/javascript"></script>
+<script src="{{ asset('dashboard/assets/plugins/jquery-scrollbar/jquery.scrollbar.min.js')}}" type="text/javascript"></script>
+<!-- END CORE JS FRAMEWORK -->
+<!-- BEGIN PAGE LEVEL JS -->
+<script src="{{ asset('dashboard/assets/plugins/jquery-slider/jquery.sidr.min.js')}}" type="text/javascript"></script>
+<script src="{{ asset('dashboard/assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js')}}" type="text/javascript"></script>
+<script src="{{ asset('dashboard/assets/plugins/pace/pace.min.js')}}" type="text/javascript"></script>
+<script src="{{ asset('dashboard/assets/plugins/jquery-numberAnimate/jquery.animateNumbers.js')}}" type="text/javascript"></script>
+<script src="{{ asset('dashboard/assets/plugins/jquery-ricksaw-chart/js/raphael-min.js')}}"></script>
+<script src="{{ asset('dashboard/assets/plugins/jquery-ricksaw-chart/js/d3.v2.js')}}"></script>
+<script src="{{ asset('dashboard/assets/plugins/jquery-ricksaw-chart/js/rickshaw.min.js')}}"></script>
+<script src="{{ asset('dashboard/assets/plugins/jquery-sparkline/jquery-sparkline.js')}}"></script>
+<script src="{{ asset('dashboard/assets/plugins/skycons/skycons.js')}}"></script>
+<script src="{{ asset('dashboard/assets/plugins/owl-carousel/owl.carousel.min.js')}}" type="text/javascript"></script>
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+<script src="{{ asset('dashboard/assets/plugins/jquery-gmap/gmaps.js')}}" type="text/javascript"></script>
+<script src="{{ asset('dashboard/assets/plugins/Mapplic/js/jquery.easing.js')}}" type="text/javascript"></script>
+<script src="{{ asset('dashboard/assets/plugins/Mapplic/js/jquery.mousewheel.js')}}" type="text/javascript"></script>
+<script src="{{ asset('dashboard/assets/plugins/Mapplic/js/hammer.js')}}" type="text/javascript"></script>
+<script src="{{ asset('dashboard/assets/plugins/Mapplic/mapplic/mapplic.js')}}" type="text/javascript"></script>
+    
+<script src="{{ asset('dashboard/assets/plugins/jquery-flot/jquery.flot.js')}}" type="text/javascript"></script>
+<script src="{{ asset('dashboard/assets/plugins/jquery-flot/jquery.flot.resize.min.js')}}" type="text/javascript"></script>
+<script src="{{ asset('dashboard/assets/plugins/jquery-metrojs/MetroJs.min.js')}}" type="text/javascript" ></script>
+<!-- END PAGE LEVEL PLUGINS -->
+<!-- BEGIN CORE TEMPLATE JS -->
+<script src="{{ asset('dashboard/assets/js/core.js')}}" type="text/javascript"></script>
+<script src="{{ asset('dashboard/assets/js/chat.js')}}" type="text/javascript"></script>
+<script src="{{ asset('dashboard/assets/js/demo.js')}}" type="text/javascript"></script>
+<script src="{{ asset('dashboard/assets/js/dashboard_v2.js')}}" type="text/javascript"></script>
+<script type="text/javascript">
+        $(document).ready(function () {
+            $(".live-tile,.flip-list").liveTile();
+        });
+</script>
+
+<!-- END CORE TEMPLATE JS -->
+</body>
+</html>
